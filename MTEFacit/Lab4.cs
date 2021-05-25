c5a7d512d701f1e83b8a1782e65b5f3b2f141e4d
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-05-25 14:49:55 +0200

    Add project files.

---
+++ MTEFacit/Lab4.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MTEFacit
+{
+    public class Lab4
+    {
+        public void Run()
+        {
+            Console.Write("Skriv in ditt förnamn:");
+
+            string fornamn = Console.ReadLine();
+
+            Console.Write("Skriv in ditt efternamn:");
+
+            string efternamn = Console.ReadLine();
+
+            Console.WriteLine(efternamn + ", " + fornamn);
+        }
+    }
+}
---
+++ MTEFacit/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MTEFacit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var lab = new Lab4();
+            lab.Run();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
